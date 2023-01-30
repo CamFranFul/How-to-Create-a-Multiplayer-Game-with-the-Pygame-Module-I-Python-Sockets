2902ea6f37b895ec9b56882c33a66d148b760251
CamFranFul <cameron.fulfer@mavs.uta.edu>
2023-01-30 16:54:27 -0600

    had the TCP client socket decode and print the message sent by the TCP server socket and closed both sockets by following along with https://www.youtube.com/watch?v=5mv9ePKCSe8 from 00:51:47 to 00:58:11

--- python_socket_examples/1_socket_intro/tcp_client.py
+++ python_socket_examples/1_socket_intro/tcp_client.py
@@ -7,3 +7,11 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # connect the socket to a server located at a given IP and port
 client_socket.connect((socket.gethostbyname(socket.gethostname()), 12345)) # we don't have to hardcode the address since the server is on the same machine as the client
+
+# receive a message from the server...you must specify the max number of bytes to receive
+message = client_socket.recv(1024) # may need to adjust the maximum number of bytes later
+print(message.decode("utf-8"))
+
+# close the client socket
+client_socket.close()
+
--- python_socket_examples/1_socket_intro/tcp_server.py
+++ python_socket_examples/1_socket_intro/tcp_server.py
@@ -6,8 +6,8 @@ import socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # see how to get IP address dynamically
-print(socket.gethostname()) # hostname
-print(socket.gethostbyname(socket.gethostname())) # IP address of the given host name
+# print(socket.gethostname()) # hostname
+# print(socket.gethostbyname(socket.gethostname())) # IP address of the given host name
 
 # bind our new socket to a tuple (IP Address, Port Address)
 server_socket.bind((socket.gethostbyname(socket.gethostname()), 12345))
@@ -19,13 +19,17 @@ server_socket.listen()
 while True:
     # accept every single connection and store two pieces of information
     client_socket, client_address = server_socket.accept() # returns a tuple of a client socket object and a tuple of that client socket object's IPv4 and port address that the client is using for the connection (the client port address being different from the server port address that he client is connecting to)
-    print(type(client_socket))
-    print(client_socket)
-    print(type(client_address))
-    print(client_address)
+    # print(type(client_socket))
+    # print(client_socket)
+    # print(type(client_address))
+    # print(client_address)
 
     print(f'Connected to {client_address}!\n')
 
     # send a message to the client that just connected
     client_socket.send('You are connected!'.encode("utf-8"))
+
+    # close the connection
+    server_socket.close()
+    break
 
