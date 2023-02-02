f162ceebef064a14343b9635a6a2e1d87c579e5e
CamFranFul <cameron.fulfer@mavs.uta.edu>
2023-02-02 00:22:39 -0600

    created a root window using Tkinter by following along with https://www.youtube.com/watch?v=5mv9ePKCSe8 from 02:42:57 to 02:54:23

---
+++ python_socket_examples/3_basic_gui_chat_room/tkinter_intro.py
@@ -0,0 +1,26 @@
+# Tkinter introduction
+import tkinter
+
+# define window
+root = tkinter.Tk()
+root.title("Let's Chat")
+root.iconbitmap('blue_talk.png')
+root.geometry('400x600')
+root.resizable(0,0)
+
+# define colors
+root_color = "#535657"
+input_color = "#4f646f"
+output_color = "dee7e7"
+root.config(bg=root_color)
+
+# define functions
+
+
+# define GUI layout
+
+
+# run the loop window's mainloop()
+root.mainloop()
+
+
