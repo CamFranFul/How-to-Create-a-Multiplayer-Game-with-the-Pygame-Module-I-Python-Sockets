aabf972490139e5eeacab5f6a841e42b9185ea42
CamFranFul <cameron.fulfer@mavs.uta.edu>
2023-02-03 12:10:03 -0600

    followed along with https://www.youtube.com/watch?v=5mv9ePKCSe8 from 03:08:43 to 03:22:50: ¶ added 3 radio button widgets (red, green, and blue) to change the message text color ¶ added a "---Stored Messages---" header for the output messages

--- python_socket_examples/3_basic_gui_chat_room/tkinter_intro.py
+++ python_socket_examples/3_basic_gui_chat_room/tkinter_intro.py
@@ -1,6 +1,6 @@
 # Tkinter introduction
 import tkinter
-from tkinter import BOTH
+from tkinter import BOTH, StringVar
 
 # define window
 root = tkinter.Tk()
@@ -10,9 +10,9 @@ root.geometry('400x600')
 root.resizable(0,0)
 
 # define colors
-root_color = "#535657"
-input_color = "#4f646f"
-output_color = "#dee7e7"
+root_color = "#535657" # R:83, G:86, B:87 that is Very dark grayish cyan
+input_color = "#4f646f" # R:79, G:100, B:111 that is Very dark grayish blue
+output_color = "#dee7e7" # R:222, G:231, B:231 that is Light grayish cyan
 root.config(bg=root_color)
 
 # define functions
@@ -26,12 +26,26 @@ input_frame.pack(pady=10)
 output_frame.pack(padx=10, pady=(0,10), fill=BOTH, expand=True)
 
 # define widgets (that are placed on the frames)
-message_entry = tkinter.Entry(input_frame, text = "Enter a message", width =30)
+message_entry = tkinter.Entry(input_frame, text = "Enter a message", width=30)
 send_button = tkinter.Button(input_frame, text="Send")
-message_entry.grid(row=0, column=0, padx=10, pady=10)
-send_button.grid(row=0, column=1, padx=10, pady=10) # did not include "ipadx=20, ipady=5" as the button gets cut off
+message_entry.grid(row=0, column=0, columnspan =3, padx=10, pady=10)
+send_button.grid(row=0, column=3, rowspan=2, padx=10, pady=10) # did not include "ipadx=20, ipady=5" as the button gets cut off
 
+text_color = StringVar() # 'StringVar()' is a class imported from tkinter module that's a type of variable with objects that are strings such as 'text_color'
+text_color.set("#ff0000") # sets 'text color' to the value red by default
+# these widgets will track the string variable object 'text_color'
+# the value of this string variable object is expressed as a string, in this case the strings are text colors
+red_button = tkinter.Radiobutton(input_frame, text="Red", variable=text_color, value="#ff0000", bg=input_color)
+green_button = tkinter.Radiobutton(input_frame, text="Green", variable=text_color, value="#ff0000", bg=input_color)
+blue_button = tkinter.Radiobutton(input_frame, text="Blue", variable=text_color, value="#ff0000", bg=input_color)
+# place these radio button widgets onto the input frame via the grid system
+red_button.grid(row=1, column=0)
+green_button.grid(row=1, column=1)
+blue_button.grid(row=1, column=2)
 
+# creates the "---Stored Messages---" header for the output messages
+output_label = tkinter.Label(output_frame, text="---Stored Messages---", fg=input_color, bg=output_color, font=('Helvetica bold', 18))
+output_label.pack(pady=15)
 # run the loop window's mainloop()
 root.mainloop()
 
